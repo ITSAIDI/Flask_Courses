0600c0d26eff6d39b91ffd832c4e632b419e1357
ITSAIDI <noureddinesaidi111@gmail.com>
2024-09-11 21:53:13 +0100

    c

--- App1/Backend/wsgi.py
+++
@@ -1,8 +0,0 @@
-from main import app
-
-app.run(host='0.0.0.0', port=5000)
-
-# pip freeze > requirements.txt
-# In render
-# pip install -r requirements.txt && cd ../Frontend && npm install && npm run build
-# gunicorn wsgi:app
